741df271251270474b058b050d7a3f0aaf74bde6
LukasDrsman <lukaskodr@gmail.com>
2020-05-14 22:16:01 +0200

    Added output modes

--- config-example.py
+++ config-example.py
@@ -10,11 +10,8 @@ ndays = 1
 # your school's website
 website = 'https://seslm.edupage.org'
 
-# 2 print or not 2 print homework into terminal
-printdata = True
-
-# homework separator
-separator = "-------------------------------------------"
+# homework separator character
+separatorchar = "-"
 
 # which webdriver to use ["Firefox" or "Chrome"]
 wdriver = "Firefox"
